aa91ccdd2052aadac50f1a6f032798b326909b0e
Michael Schenk <michaelschenk72@gmail.com>
2020-10-15 19:57:41 +0200

    Adding HP 6614C CS fix based on PIC 10F202

---
+++ hp-6614C-cs-fix/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HP6614C-CS-Fix.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HP6614C-CS-Fix.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=hp6614c-cs-fix.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hp6614c-cs-fix.x.tar
