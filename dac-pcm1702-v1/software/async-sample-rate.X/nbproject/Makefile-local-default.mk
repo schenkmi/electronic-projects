834de8c15dd4a18225c871608a8eb91421b0bdfc
Michael Schenk <michaelschenk72@gmail.com>
2021-03-25 18:47:12 +0100

    Update

--- dac-pcm1702-v1/software/async-sample-rate.X/nbproject/Makefile-local-default.mk
+++ dac-pcm1702-v1/software/async-sample-rate.X/nbproject/Makefile-local-default.mk
@@ -20,18 +20,18 @@ PATH:=/Applications/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/
 # Path to java used to run MPLAB X when this makefile was created
 MP_JAVA_PATH="/Applications/microchip/mplabx/v5.45/sys/java/zulu8.48.0.53-ca-fx-jre8.0.265-macosx_x64/zulu-8.jre/Contents/Home/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="/Applications/microchip/xc8/v2.31/bin/xc8-cc"
+MP_CC="/Applications/microchip/xc8/v2.32/bin/xc8-cc"
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="/Applications/microchip/xc8/v2.31/bin/xc8-cc"
-MP_LD="/Applications/microchip/xc8/v2.31/bin/xc8-cc"
-MP_AR="/Applications/microchip/xc8/v2.31/bin/xc8-ar"
+MP_AS="/Applications/microchip/xc8/v2.32/bin/xc8-cc"
+MP_LD="/Applications/microchip/xc8/v2.32/bin/xc8-cc"
+MP_AR="/Applications/microchip/xc8/v2.32/bin/xc8-ar"
 DEP_GEN=${MP_JAVA_PATH}java -jar "/Applications/microchip/mplabx/v5.45/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="/Applications/microchip/xc8/v2.31/bin"
+MP_CC_DIR="/Applications/microchip/xc8/v2.32/bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="/Applications/microchip/xc8/v2.31/bin"
-MP_LD_DIR="/Applications/microchip/xc8/v2.31/bin"
-MP_AR_DIR="/Applications/microchip/xc8/v2.31/bin"
+MP_AS_DIR="/Applications/microchip/xc8/v2.32/bin"
+MP_LD_DIR="/Applications/microchip/xc8/v2.32/bin"
+MP_AR_DIR="/Applications/microchip/xc8/v2.32/bin"
 # MP_BC_DIR is not defined
 DFP_DIR=/Applications/microchip/mplabx/v5.45/packs/Microchip/PIC16F1xxxx_DFP/1.5.133
--- dac-pcm1702-v1/software/async-sample-rate.X/nbproject/Makefile-variables.mk
+++ dac-pcm1702-v1/software/async-sample-rate.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=async-sample-rate.X.production.null
-CND_ARTIFACT_PATH_default=dist/default/production/async-sample-rate.X.production.null
+CND_ARTIFACT_NAME_default=async-sample-rate.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/async-sample-rate.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
 CND_PACKAGE_NAME_default=async-sample-rate.x.tar
 CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/async-sample-rate.x.tar
