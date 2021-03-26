e0f46a986cc820d7cc661b0b60f9fffd8c6422d9
Michael Schenk <michaelschenk72@gmail.com>
2021-03-26 06:43:02 +0100

    Update main.c

--- dac-pcm1702-v1/software/async-sample-rate.X/main.c
+++ dac-pcm1702-v1/software/async-sample-rate.X/main.c
@@ -42,6 +42,161 @@
 */
 
 #include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/examples/i2c1_master_example.h"
+
+// Page 38 
+#define SRC4392_I2C_SLAVE_ADDR              0x70
+
+
+// SRC4392 control/status registers
+#define SRC_REG01	0x01		// power down and reset
+#define SRC_REG02	0x02		// global interrupt status
+#define SRC_REG03	0x03		// port A control
+#define SRC_REG04	0x04		// port A control
+#define SRC_REG05	0x05		// port B control
+#define SRC_REG06	0x06		// port B control
+#define SRC_REG07	0x07		// DIT control
+#define SRC_REG08	0x08		// DIT control
+#define SRC_REG09	0x09		// DIT control
+#define SRC_REG0A	0x0a		// SRC and DIT status
+#define SRC_REG0B	0x0b		// SRC and DIT interrupt mask
+#define SRC_REG0C	0x0c		// SRC and DIT interrupt mode
+#define SRC_REG0D	0x0d		// DIR control
+#define SRC_REG0E	0x0e		// DIR control
+#define SRC_REG0F	0x0f		// DIR PLL configuration
+#define SRC_REG10	0x10		// DIR PLL configuration
+#define SRC_REG11	0x11		// DIR PLL configuration
+#define SRC_REG12	0x12		// non-PCM audio detection
+#define SRC_REG13	0x13		// DIR status
+#define SRC_REG14	0x14		// DIR status
+#define SRC_REG15	0x15		// DIR status
+#define SRC_REG16	0x16		// DIR interrupt mask
+#define SRC_REG17	0x17		// DIR interrupt mask
+#define SRC_REG18	0x18		// DIR interrupt mode
+#define SRC_REG19	0x19		// DIR interrupt mode
+#define SRC_REG1A	0x1a		// DIR interrupt mode
+#define SRC_REG1B	0x1b		// GP01
+#define SRC_REG1C	0x1c		// GP02
+#define SRC_REG1D	0x1d		// GP03
+#define SRC_REG1E	0x1e		// GP04
+#define SRC_REG1F	0x1f		// audio CD Q-channel subcode
+#define SRC_REG20	0x20		// audio CD Q-channel subcode
+#define SRC_REG21	0x21		// audio CD Q-channel subcode
+#define SRC_REG22	0x22		// audio CD Q-channel subcode
+#define SRC_REG23	0x23		// audio CD Q-channel subcode
+#define SRC_REG24	0x24		// audio CD Q-channel subcode
+#define SRC_REG25	0x25		// audio CD Q-channel subcode
+#define SRC_REG26	0x26		// audio CD Q-channel subcode
+#define SRC_REG27	0x27		// audio CD Q-channel subcode
+#define SRC_REG28	0x28		// audio CD Q-channel subcode
+#define SRC_REG29	0x29		// PC burst preamble, high byte
+#define SRC_REG2A	0x2a		// PC burst preamble, low byte
+#define SRC_REG2B	0x2b		// PD burst preamble, high byte
+#define SRC_REG2C	0x2c		// PD burst preamble, low byte
+#define SRC_REG2D	0x2d		// SRC control
+#define SRC_REG2E	0x2e		// SRC control
+#define SRC_REG2F	0x2f		// SRC control
+#define SRC_REG30	0x30		// SRC control
+#define SRC_REG31	0x31		// SRC control
+#define SRC_REG32	0x32		// SRC input/output ratio
+#define SRC_REG33	0x33		// SRC input/output ratio
+#define SRC_REG7F	0x7f		// page selection
+
+
+// Read one byte from the SRC4392
+uint8_t src4392_read(uint8_t reg)
+{
+    return I2C1_Read1ByteRegister(SRC4392_I2C_SLAVE_ADDR, reg);   
+}
+
+
+// Write one byte to the SRC4392
+void src4392_write(uint8_t reg, uint8_t val)
+{
+    I2C1_Write1ByteRegister(SRC4392_I2C_SLAVE_ADDR, reg, val);
+}
+
+
+void init(void)
+{   
+    __delay_ms(100);
+    RESET_SetLow();
+    __delay_ms(100);
+    RESET_SetHigh();
+
+	// Select page 0
+	src4392_write(SRC_REG7F, 0x00);
+
+	// DIR setup:
+	// - set default input source
+	// - use MCLK as clock source
+	// - audio not muted for loss of lock condition
+	// - PLL stops for loss of lock condition
+	// - RXCKO output disabled
+	src4392_write(SRC_REG0D, 0x08);	// default input source = RX1
+	src4392_write(SRC_REG0E, 0x00);
+
+	// PLL1 setup:
+	// 24.576MHz, p = 2, j = 8, d = 0
+	src4392_write(SRC_REG0F, 0x22);
+	src4392_write(SRC_REG10, 0x00);
+	src4392_write(SRC_REG11, 0x00);
+
+	// Set GP01 for DIR non-audio flag
+	src4392_write(SRC_REG1B, 0x06);
+	// Set GP02 for DIR non-valid flag
+	src4392_write(SRC_REG1C, 0x07);
+	// Set GP03 for DIR emphasis flag
+	src4392_write(SRC_REG1D, 0x05);
+	// Set GP04 for DIR parity error flag
+	src4392_write(SRC_REG1E, 0x0d);
+
+	// SRC setup:
+	// - DIR as the input data source
+	// - use MCLK as clock source
+	// - autodem enabled
+	src4392_write(SRC_REG2D, 0x02);	// default input source = DIR
+	src4392_write(SRC_REG2E, 0x20);
+	src4392_write(SRC_REG2F, 0x00);
+	src4392_write(SRC_REG30, 0x00);
+	src4392_write(SRC_REG31, 0x00);
+
+	// DIT setup:
+	// - SRC as the input data source
+	// - MCLK as master clock
+	// - clock divider 128 to set the output frame rate
+	// - block start is an output and valid audio is indicated
+	// - c and u data will not be updated
+	src4392_write(SRC_REG07, 0x1c);
+	src4392_write(SRC_REG08, 0x00);
+	src4392_write(SRC_REG09, 0x00);
+
+	// set up for 192KHz output
+	src4392_write(SRC_REG08, 0x08);
+	src4392_write(SRC_REG7F, 0x02);
+	src4392_write(0x00, 0x80);
+	src4392_write(0x01, 0x80);
+	src4392_write(0x08, 0x18);
+	src4392_write(0x09, 0x18);
+	src4392_write(SRC_REG7F, 0x00);
+	src4392_write(SRC_REG08, 0x00);
+
+	// Power up all blocks
+	src4392_write(SRC_REG01, 0x3f);
+
+	// Port A setup:
+	// - master with i2s data format
+	// - SRC as input data source
+	// - MCLK as clock source
+	// - clock divider 128
+	src4392_write(SRC_REG03, 0x39);
+	src4392_write(SRC_REG04, 0x00);
+
+	// Port B setup:
+	// - slave with i2s data format
+	src4392_write(SRC_REG05, 0x01);
+	src4392_write(SRC_REG06, 0x00);
+}
 
 /*
                          Main application
@@ -66,9 +221,10 @@ void main(void)
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
 
-    while (1)
-    {
-        // Add your application code
+    init();
+    
+    while (1) {
+        __delay_ms(20);
     }
 }
 /**
