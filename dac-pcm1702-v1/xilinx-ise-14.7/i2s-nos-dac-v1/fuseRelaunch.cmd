1c62589c99bdd15f5bca520c36dce8e24f1dfb9b
Michael Schenk <michaelschenk72@gmail.com>
2021-06-24 20:01:17 +0200

    Adding VHDL I2S to NOS DAC for XC2C64A-xVQ44

---
+++ dac-pcm1702-v1/xilinx-ise-14.7/i2s-nos-dac-v1/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/schenk/Documents/GitHub/electronic-projects/xilinx-cpld-demo-v1/ise-14.7/i2s-nos-dac/i2s_nos_isim_beh.exe" -prj "C:/Users/schenk/Documents/GitHub/electronic-projects/xilinx-cpld-demo-v1/ise-14.7/i2s-nos-dac/i2s_nos_beh.prj" "work.i2s_nos" 
