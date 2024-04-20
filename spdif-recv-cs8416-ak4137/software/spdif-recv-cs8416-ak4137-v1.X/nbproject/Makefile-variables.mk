1904bbb1720ed93877c513ec8cb469a7e67f4ff2
Michael Schenk <michaelschenk72@gmail.com>
2024-04-20 17:56:56 +0200

    wip

---
+++ spdif-recv-cs8416-ak4137/software/spdif-recv-cs8416-ak4137-v1.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=spdif-recv-cs8416-ak4137-v1.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/spdif-recv-cs8416-ak4137-v1.X.production.hex
