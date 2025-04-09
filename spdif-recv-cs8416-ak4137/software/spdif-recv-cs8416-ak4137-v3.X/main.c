2792a916e604b0aa73d9b46fd13f285137fef21f
Michael Schenk <michaelschenk72@gmail.com>
2025-04-09 13:14:37 +0200

    V3 to support PCM1704 24Bit

---
+++ spdif-recv-cs8416-ak4137/software/spdif-recv-cs8416-ak4137-v3.X/main.c
@@ -0,0 +1,228 @@
+/**
+ * PIC16F18056 based async sample rate converter
+ * for CS8416 / AK4137
+ *
+ * Copyright (c) 2024-2025, Michael Schenk
+ * All Rights Reserved
+ *
+ * Author: Michael Schenk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * OEMs, ISVs, VARs and other distributors that combine and distribute
+ * commercially licensed software with Michael Schenks software
+ * and do not wish to distribute the source code for the commercially
+ * licensed software under version 2, or (at your option) any later
+ * version, of the GNU General Public License (the "GPL") must enter
+ * into a commercial license agreement with Michael Schenk.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file LICENSE.txt. If not, write to
+ * the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+ /**
+  * Save hex
+  * cd /work/electronic-projects/spdif-recv-cs8416-ak4137/software
+  * cp spdif-recv-cs8416-ak4137-v2.X/dist/default/production/spdif-recv-cs8416-ak4137-v2.X.production.hex hex/
+  */
+
+/**
+ * History
+ * V3.0     2025.04.09 PCM1704 drop
+ * V1.1     2024.06.05 PCM1702 drop
+ * V1.0     2024.04.21 Start develop
+ */
+
+#include "mcc_generated_files/system/system.h"
+#include "rotary_encoder.h"
+#include "cs8416.h"
+#include "ak4137.h"
+#include "pcm1792a.h"
+
+//#define __USE_PCM1792A__
+//#define __USE_PCM1702__
+#define __USE_PCM1704__
+
+#define STARTUP_WAIT                    250 /* wait 250ms after SYSTEM_Initialize */
+
+/* eeprom initialize 0x00..0x07 */
+__EEPROM_DATA(ROTARY_MIN_ATTENUATION /* channel 0 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 1 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 2 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 3 attenuation initial */,
+              ROTARY_MIN_CHANNEL     /* channel selection initial     */,
+              0xff, 0xff, 0xff);
+
+
+volatile Instance_t instance = {
+  .mode = Single, .channel = -1, .last_channel = -1,
+  .attenuation = -1, .last_attenuation = -1,
+  .eeprom_save_status_counter = -1,
+  .channel_attenuation = {
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+  },
+  .control =  Channel,
+  .encoder = {
+    { .direction = DIR_NONE,  .encoder_count = { 0, 0 },  .rotary_encoder_state = 0,
+      .encoder_push_debounce_counter = 0, .encoder_push_counter = 0, .encoder_push_action = 0  },
+    { .direction = DIR_NONE,  .encoder_count = { 0, 0 },  .rotary_encoder_state = 0,
+      .encoder_push_debounce_counter = 0, .encoder_push_counter = 0, .encoder_push_action = 0  },
+  },
+};
+
+CS8416_t cs8416 = {
+    .output_format = CS_I2S,
+    .output_word_length = CS_OWL24Bit,
+};
+
+AK4137_t ak4137 = {
+    .input_format = AK_I2S32or16Bit,
+    .digital_filter = AK_ShortDelaySharpRollOff,
+    .output_format = AK_I2S,
+    .output_sampling_frequency = AK_FS384kHz, 
+#ifdef __USE_PCM1702__
+    .output_word_length = AK_OWL20Bit, 
+#else
+    .output_word_length = AK_OWL24Bit,
+#endif
+};
+
+#ifdef __USE_PCM1792A__
+PCM1792A_t pcm1792a = {
+    .filter_rolloff = TI_Slow,
+};
+#endif
+
+static void init(volatile Instance_t* instance)
+{
+    LED_D3_SetHigh();
+    LED_D4_SetHigh();
+    
+    /* External Oscillator Selection bits: Oscillator not enabled otherwise RA7 is CLKIN and LED D5 is not working*/
+    LED_D5_SetHigh();
+
+    ak4137_preinit(&ak4137);
+    
+    __delay_ms(100);
+    RESET_SetLow();
+    __delay_ms(100);
+    RESET_SetHigh();
+    __delay_ms(10);
+
+    cs8416_init(&cs8416);
+
+    ak4137_init(&ak4137);
+    
+#ifdef __USE_PCM1792A__
+    pcm1792a_init(&pcm1792a);
+#endif
+
+    /* read last used channel, channels attenuation will be handler inside process_channel() */
+    instance->channel = eeprom_read(EEPROM_ADDR_CHANNEL);
+
+    /* read default attenuation for each channel and assign to channel attenuation */
+    for (uint8_t cnt = 0; cnt <= ROTARY_MAX_CHANNEL; cnt++) {
+        instance->channel_attenuation[cnt].attenuation = instance->channel_attenuation[cnt].default_attenuation = eeprom_read(cnt);
+    }
+}
+
+/* Factory reset */
+static void factory_reset() {
+    if (ENC1_SWITCH_GetValue() == 0) {
+        while(ENC1_SWITCH_GetValue() == 0) {
+            __delay_ms(100);  
+        }
+
+        for (int cnt = 0; cnt < 10; cnt++) {
+            /* LED on */
+            LED_D4_SetDigitalInput();
+             __delay_ms(250);  
+            /* LED off */
+            LED_D4_SetDigitalOutput();
+            __delay_ms(250);  
+        }
+
+        eeprom_write(0x00, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x01, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x02, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x03, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x04, ROTARY_MIN_CHANNEL);
+    }
+}
+
+void test_timer_callback(void) {   
+    LED_D5_Toggle();
+}
+
+/* channel selection relay are on RB0...RB3 */
+static void process_channel(volatile Instance_t* instance)
+{
+  if (instance->channel != instance->last_channel)  {
+    if (instance->last_channel != -1) {
+      /* store current used attenuation on channel */
+      instance->channel_attenuation[instance->last_channel].attenuation = instance->attenuation;
+    }
+
+    cs8416_set_input(&cs8416, instance->channel);
+
+    instance->last_channel = instance->channel;
+    instance->eeprom_save_status_counter = EEPROM_SAVE_STATUS_VALUE;
+  }
+}
+
+/* attenuator relay are on RA0...RA5 */
+static void process_attenuation(volatile Instance_t* instance) {
+  if (instance->attenuation != instance->last_attenuation) {
+      instance->last_attenuation = instance->attenuation;
+  }
+}
+
+/**
+ * Main application
+ */
+int main(void)
+{
+    SYSTEM_Initialize();
+
+    __delay_ms(STARTUP_WAIT);  
+    
+    /* if rotary enc board not connected, don't call this */
+    factory_reset();
+
+    /* install irq handlers */
+    TMR0_OverflowCallbackRegister(rotary_encoder_timer_callback);
+ 
+    /* Enable the Global Interrupts */
+    INTERRUPT_GlobalInterruptEnable();
+
+    /* Enable the Peripheral Interrupts */
+    INTERRUPT_PeripheralInterruptEnable();
+
+    TMR0_Start();
+    
+    /* IRQs need to be enabled for I2C */
+    init(&instance);
+
+    while (1) {
+      process_channel(&instance);
+      process_attenuation(&instance);
+      process_encoder_button(&instance);
+      eeprom_save_status(&instance);
+      __delay_ms(MAIN_LOOP_WAIT);
+    } 
+}
---
+++ spdif-recv-cs8416-ak4137/software/spdif-recv-cs8416-ak4137-v3.X/rotary_encoder.h
@@ -0,0 +1,104 @@
+/**
+ * PIC16F18056 based async sample rate converter
+ * for CS8416 / AK4137
+ *
+ * Copyright (c) 2024-2024, Michael Schenk
+ * All Rights Reserved
+ *
+ * Author: Michael Schenk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * OEMs, ISVs, VARs and other distributors that combine and distribute
+ * commercially licensed software with Michael Schenks software
+ * and do not wish to distribute the source code for the commercially
+ * licensed software under version 2, or (at your option) any later
+ * version, of the GNU General Public License (the "GPL") must enter
+ * into a commercial license agreement with Michael Schenk.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file LICENSE.txt. If not, write to
+ * the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+#include <stdint.h>
+
+#pragma once
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define DIR_NONE  0x00 /* read() return value - No complete step/movement */
+#define DIR_CW    0x10 /* read() return value - Clockwise step/movement */
+#define DIR_CCW   0x20 /* return value - Counter-clockwise step/movement */
+
+#define CHAN_SEL_MASK                  0x0f
+
+#define EEPROM_ADDR_CHANNEL            0x04 /* EEPROM address of current channel */
+
+#define ROTARY_MIN_CHANNEL                0 /* minimum channel */
+#define ROTARY_MAX_CHANNEL                3 /* maximum channel */
+#define ROTARY_MULTI_CHANNEL              3 /* on 12PPR this gaves 3 clicks */
+
+#define ROTARY_ATTENUATION_BITS           8 /* 8 bits */
+#define ROTARY_MIN_ATTENUATION            0 /* minimum attenuation */
+#define ROTARY_MAX_ATTENUATION         ((1 << ROTARY_ATTENUATION_BITS) - 1) /* (0xff) maximum attenuation */
+#define ROTARY_MULTI_ATTENUATION          1 /* on 12PPR this gaves 1 clicks */
+
+#define MAIN_LOOP_WAIT                    1 /* 1ms */
+#define EEPROM_SAVE_STATUS_VALUE       1000 /* 1 seconds on a 1ms loop */
+#define RELAIS_SETUP_TIME                 1 /* 1ms */
+
+#define ROTARY_PUSH_DEBOUNCE             20 /* 20 ms on a 1ms timer IRQ */
+#define STORE_DEFAULT_ATTENUATION_TIME ((3 /* seconds */ * 1000) / ROTARY_PUSH_DEBOUNCE) /* 3 seconds till storing default attenuation */
+
+enum Control { Combined = 0, Volume = 0, Channel = 1};
+enum Mode { Single = 0, Dual = 1 };
+
+typedef struct {
+  uint8_t direction;
+  int encoder_count[2 /* Volume = 0,  Channel = 1 */];
+  /* rotary encoder state */
+  uint8_t rotary_encoder_state;
+  /* encoder push button */
+  int encoder_push_debounce_counter;
+  int encoder_push_counter;
+  int encoder_push_action;
+} RotaryEncoder_t;
+
+typedef struct {
+  int default_attenuation;
+  int attenuation;
+} ChannelVolume_t;
+
+typedef struct {
+  enum Mode mode; /* single or dual encoder mode */
+  int channel;
+  int last_channel;
+  int attenuation;
+  int last_attenuation;
+  int eeprom_save_status_counter;
+  ChannelVolume_t channel_attenuation[ROTARY_MAX_CHANNEL + 1]; /* channel 0..3 */
+  /* irq changed */
+  volatile enum Control control;
+  RotaryEncoder_t encoder[2 /* 0 = Combined/Volume, 1 = Channel */];
+} Instance_t;
+
+void eeprom_save_status(volatile Instance_t* instance);
+void process_encoder_button(volatile Instance_t* instance);
+void rotary_encoder_timer_callback(void);
+
+#ifdef	__cplusplus
+}
+#endif
