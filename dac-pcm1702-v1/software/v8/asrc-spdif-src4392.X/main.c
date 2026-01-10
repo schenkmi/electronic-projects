1674def8a0897621fedbb2d245a6919af08a8a6c
Michael Schenk <michaelschenk72@gmail.com>
2026-01-10 17:24:09 +0100

    Start with v8 updated MCC code

---
+++ dac-pcm1702-v1/software/v8/asrc-spdif-src4392.X/main.c
@@ -0,0 +1,226 @@
+/**
+ * PIC16F18056 based async sample rate converter
+ * for SRC4392
+ *
+ * Copyright (c) 2024-2025, Michael Schenk
+ * All Rights Reserved
+ *
+ * Author: Michael Schenk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * OEMs, ISVs, VARs and other distributors that combine and distribute
+ * commercially licensed software with Michael Schenks software
+ * and do not wish to distribute the source code for the commercially
+ * licensed software under version 2, or (at your option) any later
+ * version, of the GNU General Public License (the "GPL") must enter
+ * into a commercial license agreement with Michael Schenk.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file LICENSE.txt. If not, write to
+ * the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
+ /**
+  * Save hex
+  * cd /work/electronic-projects/spdif-recv-cs8416-ak4137/software
+  * cp spdif-recv-cs8416-ak4137-v2.X/dist/default/production/spdif-recv-cs8416-ak4137-v2.X.production.hex hex/
+  */
+
+/**
+ * History
+ * V7.0     2025.05.04 Using PIC16F18056-I/SS MPLAB-X 6.25
+ */
+
+#include "mcc_generated_files/system/system.h"
+#include "rotary_encoder.h"
+#include "src4392.h"
+#include "pcm1792a.h"
+
+//#define __USE_PCM1792A__
+//#define __USE_PCM1702__
+#define __USE_PCM1704__
+
+#define STARTUP_WAIT                    250 /* wait 250ms after SYSTEM_Initialize */
+
+/* eeprom initialize 0x00..0x07 */
+__EEPROM_DATA(ROTARY_MIN_ATTENUATION /* channel 0 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 1 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 2 attenuation initial */,
+              ROTARY_MIN_ATTENUATION /* channel 3 attenuation initial */,
+              ROTARY_MIN_CHANNEL     /* channel selection initial     */,
+              0xff, 0xff, 0xff);
+
+
+volatile Instance_t instance = {
+  .mode = Single, .channel = -1, .last_channel = -1,
+  .attenuation = -1, .last_attenuation = -1,
+  .eeprom_save_status_counter = -1,
+  .channel_attenuation = {
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+    { .default_attenuation = ROTARY_MIN_ATTENUATION, .attenuation = -1 },
+  },
+  .control =  Channel,
+  .encoder = {
+    { .direction = DIR_NONE,  .encoder_count = { 0, 0 },  .rotary_encoder_state = 0,
+      .encoder_push_debounce_counter = 0, .encoder_push_counter = 0, .encoder_push_action = 0  },
+    { .direction = DIR_NONE,  .encoder_count = { 0, 0 },  .rotary_encoder_state = 0,
+      .encoder_push_debounce_counter = 0, .encoder_push_counter = 0, .encoder_push_action = 0  },
+  },
+};
+
+SRC4392_t src4392 = {
+    .deemphases = DeEmphasisAuto,
+    .digital_audio_interface_transmitter = DITUpsample,
+    .upsample_rate = UpsamplingTo192kHz,
+#ifdef __USE_PCM1702__
+    .output_word_length = OWL20Bit,
+#else
+    .output_word_length = OWL24Bit,
+#endif
+};
+
+#ifdef __USE_PCM1792A__
+PCM1792A_t pcm1792a = {
+    .filter_rolloff = TI_Slow,
+};
+#endif
+
+
+static void init(volatile Instance_t* instance)
+{
+
+    LED_RA5_SetHigh();
+    __delay_ms(500);
+    LED_RA5_SetLow();
+        LED_RA7_SetHigh();
+    __delay_ms(500);
+    
+    LED_RA7_SetLow();
+            LED_RA6_SetHigh();
+    __delay_ms(500);
+    LED_RA6_SetLow();
+
+    
+
+    
+    __delay_ms(100);
+    RESET_SetLow();
+    __delay_ms(100);
+    RESET_SetHigh();
+    __delay_ms(10);
+
+    src4392_init(&src4392);
+    
+#ifdef __USE_PCM1792A__
+    pcm1792a_init(&pcm1792a);
+#endif
+    /* read last used channel, channels attenuation will be handler inside process_channel() */
+    instance->channel = eeprom_read(EEPROM_ADDR_CHANNEL);
+
+    /* read default attenuation for each channel and assign to channel attenuation */
+    for (uint8_t cnt = 0; cnt <= ROTARY_MAX_CHANNEL; cnt++) {
+        instance->channel_attenuation[cnt].attenuation = instance->channel_attenuation[cnt].default_attenuation = eeprom_read(cnt);
+    }
+}
+
+
+/* Factory reset */
+static void factory_reset() {
+    if (ENC1_SWITCH_GetValue() == 0) {
+        while(ENC1_SWITCH_GetValue() == 0) {
+            __delay_ms(100);  
+        }
+
+        for (int cnt = 0; cnt < 10; cnt++) {
+            /* LED on */
+            LED_RA5_SetHigh();
+             __delay_ms(250);  
+            /* LED off */
+            LED_RA5_SetLow();
+            __delay_ms(250);  
+        }
+
+        eeprom_write(0x00, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x01, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x02, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x03, ROTARY_MIN_ATTENUATION);
+        eeprom_write(0x04, ROTARY_MIN_CHANNEL);
+    }
+}
+
+void test_timer_callback(void) {   
+    LED_RA5_Toggle();
+}
+
+static void process_channel(volatile Instance_t* instance)
+{
+  if (instance->channel != instance->last_channel)  {
+    if (instance->last_channel != -1) {
+      /* store current used attenuation on channel */
+      instance->channel_attenuation[instance->last_channel].attenuation = instance->attenuation;
+    }
+
+
+    src4392_set_input( instance->channel);
+
+    instance->last_channel = instance->channel;
+    instance->eeprom_save_status_counter = EEPROM_SAVE_STATUS_VALUE;
+  }
+}
+
+
+static void process_attenuation(volatile Instance_t* instance) {
+  if (instance->attenuation != instance->last_attenuation) {
+      instance->last_attenuation = instance->attenuation;
+  }
+}
+
+/**
+ * Main application
+ */
+int main(void)
+{
+    /* External Oscillator Selection bits: Oscillator not enabled otherwise RA5 is CLKIN */
+    SYSTEM_Initialize();
+
+    __delay_ms(STARTUP_WAIT);  
+    
+    /* if rotary enc board not connected, don't call this */
+    factory_reset();
+
+    /* install irq handlers */
+    TMR0_OverflowCallbackRegister(rotary_encoder_timer_callback);
+ 
+    /* Enable the Global Interrupts */
+    INTERRUPT_GlobalInterruptEnable();
+
+    /* Enable the Peripheral Interrupts */
+    INTERRUPT_PeripheralInterruptEnable();
+
+    TMR0_Start();
+    
+    /* IRQs need to be enabled for I2C */
+    init(&instance);
+
+    while (1) {
+      process_channel(&instance);
+      process_attenuation(&instance);
+      process_encoder_button(&instance);
+      eeprom_save_status(&instance);
+      __delay_ms(MAIN_LOOP_WAIT);
+    } 
+}
---
+++ dac-pcm1702-v1/software/v8/asrc-spdif-src4392.X/mcc_generated_files/system/pins.h
@@ -0,0 +1,243 @@
+/**
+ * Generated Pins header File
+ * 
+ * @file pins.h
+ * 
+ * @defgroup  pinsdriver Pins Driver
+ * 
+ * @brief This is generated driver header for pins. 
+ *        This header file provides APIs for all pins selected in the GUI.
+ *
+ * @version Driver Version  3.0.0
+*/
+
+/*
+? [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#ifndef PINS_H
+#define PINS_H
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set IO_RA5 aliases
+#define LED_RA5_TRIS                 TRISAbits.TRISA5
+#define LED_RA5_LAT                  LATAbits.LATA5
+#define LED_RA5_PORT                 PORTAbits.RA5
+#define LED_RA5_WPU                  WPUAbits.WPUA5
+#define LED_RA5_OD                   ODCONAbits.ODCA5
+#define LED_RA5_ANS                  ANSELAbits.ANSA5
+#define LED_RA5_SetHigh()            do { LATAbits.LATA5 = 1; } while(0)
+#define LED_RA5_SetLow()             do { LATAbits.LATA5 = 0; } while(0)
+#define LED_RA5_Toggle()             do { LATAbits.LATA5 = ~LATAbits.LATA5; } while(0)
+#define LED_RA5_GetValue()           PORTAbits.RA5
+#define LED_RA5_SetDigitalInput()    do { TRISAbits.TRISA5 = 1; } while(0)
+#define LED_RA5_SetDigitalOutput()   do { TRISAbits.TRISA5 = 0; } while(0)
+#define LED_RA5_SetPullup()          do { WPUAbits.WPUA5 = 1; } while(0)
+#define LED_RA5_ResetPullup()        do { WPUAbits.WPUA5 = 0; } while(0)
+#define LED_RA5_SetPushPull()        do { ODCONAbits.ODCA5 = 0; } while(0)
+#define LED_RA5_SetOpenDrain()       do { ODCONAbits.ODCA5 = 1; } while(0)
+#define LED_RA5_SetAnalogMode()      do { ANSELAbits.ANSA5 = 1; } while(0)
+#define LED_RA5_SetDigitalMode()     do { ANSELAbits.ANSA5 = 0; } while(0)
+// get/set IO_RA6 aliases
+#define LED_RA6_TRIS                 TRISAbits.TRISA6
+#define LED_RA6_LAT                  LATAbits.LATA6
+#define LED_RA6_PORT                 PORTAbits.RA6
+#define LED_RA6_WPU                  WPUAbits.WPUA6
+#define LED_RA6_OD                   ODCONAbits.ODCA6
+#define LED_RA6_ANS                  ANSELAbits.ANSA6
+#define LED_RA6_SetHigh()            do { LATAbits.LATA6 = 1; } while(0)
+#define LED_RA6_SetLow()             do { LATAbits.LATA6 = 0; } while(0)
+#define LED_RA6_Toggle()             do { LATAbits.LATA6 = ~LATAbits.LATA6; } while(0)
+#define LED_RA6_GetValue()           PORTAbits.RA6
+#define LED_RA6_SetDigitalInput()    do { TRISAbits.TRISA6 = 1; } while(0)
+#define LED_RA6_SetDigitalOutput()   do { TRISAbits.TRISA6 = 0; } while(0)
+#define LED_RA6_SetPullup()          do { WPUAbits.WPUA6 = 1; } while(0)
+#define LED_RA6_ResetPullup()        do { WPUAbits.WPUA6 = 0; } while(0)
+#define LED_RA6_SetPushPull()        do { ODCONAbits.ODCA6 = 0; } while(0)
+#define LED_RA6_SetOpenDrain()       do { ODCONAbits.ODCA6 = 1; } while(0)
+#define LED_RA6_SetAnalogMode()      do { ANSELAbits.ANSA6 = 1; } while(0)
+#define LED_RA6_SetDigitalMode()     do { ANSELAbits.ANSA6 = 0; } while(0)
+// get/set IO_RA7 aliases
+#define LED_RA7_TRIS                 TRISAbits.TRISA7
+#define LED_RA7_LAT                  LATAbits.LATA7
+#define LED_RA7_PORT                 PORTAbits.RA7
+#define LED_RA7_WPU                  WPUAbits.WPUA7
+#define LED_RA7_OD                   ODCONAbits.ODCA7
+#define LED_RA7_ANS                  ANSELAbits.ANSA7
+#define LED_RA7_SetHigh()            do { LATAbits.LATA7 = 1; } while(0)
+#define LED_RA7_SetLow()             do { LATAbits.LATA7 = 0; } while(0)
+#define LED_RA7_Toggle()             do { LATAbits.LATA7 = ~LATAbits.LATA7; } while(0)
+#define LED_RA7_GetValue()           PORTAbits.RA7
+#define LED_RA7_SetDigitalInput()    do { TRISAbits.TRISA7 = 1; } while(0)
+#define LED_RA7_SetDigitalOutput()   do { TRISAbits.TRISA7 = 0; } while(0)
+#define LED_RA7_SetPullup()          do { WPUAbits.WPUA7 = 1; } while(0)
+#define LED_RA7_ResetPullup()        do { WPUAbits.WPUA7 = 0; } while(0)
+#define LED_RA7_SetPushPull()        do { ODCONAbits.ODCA7 = 0; } while(0)
+#define LED_RA7_SetOpenDrain()       do { ODCONAbits.ODCA7 = 1; } while(0)
+#define LED_RA7_SetAnalogMode()      do { ANSELAbits.ANSA7 = 1; } while(0)
+#define LED_RA7_SetDigitalMode()     do { ANSELAbits.ANSA7 = 0; } while(0)
+// get/set IO_RB5 aliases
+#define RESET_TRIS                 TRISBbits.TRISB5
+#define RESET_LAT                  LATBbits.LATB5
+#define RESET_PORT                 PORTBbits.RB5
+#define RESET_WPU                  WPUBbits.WPUB5
+#define RESET_OD                   ODCONBbits.ODCB5
+#define RESET_ANS                  ANSELBbits.ANSB5
+#define RESET_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define RESET_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define RESET_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define RESET_GetValue()           PORTBbits.RB5
+#define RESET_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define RESET_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define RESET_SetPullup()          do { WPUBbits.WPUB5 = 1; } while(0)
+#define RESET_ResetPullup()        do { WPUBbits.WPUB5 = 0; } while(0)
+#define RESET_SetPushPull()        do { ODCONBbits.ODCB5 = 0; } while(0)
+#define RESET_SetOpenDrain()       do { ODCONBbits.ODCB5 = 1; } while(0)
+#define RESET_SetAnalogMode()      do { ANSELBbits.ANSB5 = 1; } while(0)
+#define RESET_SetDigitalMode()     do { ANSELBbits.ANSB5 = 0; } while(0)
+// get/set IO_RC0 aliases
+#define ENC1_CHANA_TRIS                 TRISCbits.TRISC0
+#define ENC1_CHANA_LAT                  LATCbits.LATC0
+#define ENC1_CHANA_PORT                 PORTCbits.RC0
+#define ENC1_CHANA_WPU                  WPUCbits.WPUC0
+#define ENC1_CHANA_OD                   ODCONCbits.ODCC0
+#define ENC1_CHANA_ANS                  ANSELCbits.ANSC0
+#define ENC1_CHANA_SetHigh()            do { LATCbits.LATC0 = 1; } while(0)
+#define ENC1_CHANA_SetLow()             do { LATCbits.LATC0 = 0; } while(0)
+#define ENC1_CHANA_Toggle()             do { LATCbits.LATC0 = ~LATCbits.LATC0; } while(0)
+#define ENC1_CHANA_GetValue()           PORTCbits.RC0
+#define ENC1_CHANA_SetDigitalInput()    do { TRISCbits.TRISC0 = 1; } while(0)
+#define ENC1_CHANA_SetDigitalOutput()   do { TRISCbits.TRISC0 = 0; } while(0)
+#define ENC1_CHANA_SetPullup()          do { WPUCbits.WPUC0 = 1; } while(0)
+#define ENC1_CHANA_ResetPullup()        do { WPUCbits.WPUC0 = 0; } while(0)
+#define ENC1_CHANA_SetPushPull()        do { ODCONCbits.ODCC0 = 0; } while(0)
+#define ENC1_CHANA_SetOpenDrain()       do { ODCONCbits.ODCC0 = 1; } while(0)
+#define ENC1_CHANA_SetAnalogMode()      do { ANSELCbits.ANSC0 = 1; } while(0)
+#define ENC1_CHANA_SetDigitalMode()     do { ANSELCbits.ANSC0 = 0; } while(0)
+// get/set IO_RC1 aliases
+#define ENC1_CHANB_TRIS                 TRISCbits.TRISC1
+#define ENC1_CHANB_LAT                  LATCbits.LATC1
+#define ENC1_CHANB_PORT                 PORTCbits.RC1
+#define ENC1_CHANB_WPU                  WPUCbits.WPUC1
+#define ENC1_CHANB_OD                   ODCONCbits.ODCC1
+#define ENC1_CHANB_ANS                  ANSELCbits.ANSC1
+#define ENC1_CHANB_SetHigh()            do { LATCbits.LATC1 = 1; } while(0)
+#define ENC1_CHANB_SetLow()             do { LATCbits.LATC1 = 0; } while(0)
+#define ENC1_CHANB_Toggle()             do { LATCbits.LATC1 = ~LATCbits.LATC1; } while(0)
+#define ENC1_CHANB_GetValue()           PORTCbits.RC1
+#define ENC1_CHANB_SetDigitalInput()    do { TRISCbits.TRISC1 = 1; } while(0)
+#define ENC1_CHANB_SetDigitalOutput()   do { TRISCbits.TRISC1 = 0; } while(0)
+#define ENC1_CHANB_SetPullup()          do { WPUCbits.WPUC1 = 1; } while(0)
+#define ENC1_CHANB_ResetPullup()        do { WPUCbits.WPUC1 = 0; } while(0)
+#define ENC1_CHANB_SetPushPull()        do { ODCONCbits.ODCC1 = 0; } while(0)
+#define ENC1_CHANB_SetOpenDrain()       do { ODCONCbits.ODCC1 = 1; } while(0)
+#define ENC1_CHANB_SetAnalogMode()      do { ANSELCbits.ANSC1 = 1; } while(0)
+#define ENC1_CHANB_SetDigitalMode()     do { ANSELCbits.ANSC1 = 0; } while(0)
+// get/set IO_RC2 aliases
+#define ENC1_SWITCH_TRIS                 TRISCbits.TRISC2
+#define ENC1_SWITCH_LAT                  LATCbits.LATC2
+#define ENC1_SWITCH_PORT                 PORTCbits.RC2
+#define ENC1_SWITCH_WPU                  WPUCbits.WPUC2
+#define ENC1_SWITCH_OD                   ODCONCbits.ODCC2
+#define ENC1_SWITCH_ANS                  ANSELCbits.ANSC2
+#define ENC1_SWITCH_SetHigh()            do { LATCbits.LATC2 = 1; } while(0)
+#define ENC1_SWITCH_SetLow()             do { LATCbits.LATC2 = 0; } while(0)
+#define ENC1_SWITCH_Toggle()             do { LATCbits.LATC2 = ~LATCbits.LATC2; } while(0)
+#define ENC1_SWITCH_GetValue()           PORTCbits.RC2
+#define ENC1_SWITCH_SetDigitalInput()    do { TRISCbits.TRISC2 = 1; } while(0)
+#define ENC1_SWITCH_SetDigitalOutput()   do { TRISCbits.TRISC2 = 0; } while(0)
+#define ENC1_SWITCH_SetPullup()          do { WPUCbits.WPUC2 = 1; } while(0)
+#define ENC1_SWITCH_ResetPullup()        do { WPUCbits.WPUC2 = 0; } while(0)
+#define ENC1_SWITCH_SetPushPull()        do { ODCONCbits.ODCC2 = 0; } while(0)
+#define ENC1_SWITCH_SetOpenDrain()       do { ODCONCbits.ODCC2 = 1; } while(0)
+#define ENC1_SWITCH_SetAnalogMode()      do { ANSELCbits.ANSC2 = 1; } while(0)
+#define ENC1_SWITCH_SetDigitalMode()     do { ANSELCbits.ANSC2 = 0; } while(0)
+// get/set IO_RC6 aliases
+#define IO_RC6_TRIS                 TRISCbits.TRISC6
+#define IO_RC6_LAT                  LATCbits.LATC6
+#define IO_RC6_PORT                 PORTCbits.RC6
+#define IO_RC6_WPU                  WPUCbits.WPUC6
+#define IO_RC6_OD                   ODCONCbits.ODCC6
+#define IO_RC6_ANS                  ANSELCbits.ANSC6
+#define IO_RC6_SetHigh()            do { LATCbits.LATC6 = 1; } while(0)
+#define IO_RC6_SetLow()             do { LATCbits.LATC6 = 0; } while(0)
+#define IO_RC6_Toggle()             do { LATCbits.LATC6 = ~LATCbits.LATC6; } while(0)
+#define IO_RC6_GetValue()           PORTCbits.RC6
+#define IO_RC6_SetDigitalInput()    do { TRISCbits.TRISC6 = 1; } while(0)
+#define IO_RC6_SetDigitalOutput()   do { TRISCbits.TRISC6 = 0; } while(0)
+#define IO_RC6_SetPullup()          do { WPUCbits.WPUC6 = 1; } while(0)
+#define IO_RC6_ResetPullup()        do { WPUCbits.WPUC6 = 0; } while(0)
+#define IO_RC6_SetPushPull()        do { ODCONCbits.ODCC6 = 0; } while(0)
+#define IO_RC6_SetOpenDrain()       do { ODCONCbits.ODCC6 = 1; } while(0)
+#define IO_RC6_SetAnalogMode()      do { ANSELCbits.ANSC6 = 1; } while(0)
+#define IO_RC6_SetDigitalMode()     do { ANSELCbits.ANSC6 = 0; } while(0)
+// get/set IO_RC7 aliases
+#define IO_RC7_TRIS                 TRISCbits.TRISC7
+#define IO_RC7_LAT                  LATCbits.LATC7
+#define IO_RC7_PORT                 PORTCbits.RC7
+#define IO_RC7_WPU                  WPUCbits.WPUC7
+#define IO_RC7_OD                   ODCONCbits.ODCC7
+#define IO_RC7_ANS                  ANSELCbits.ANSC7
+#define IO_RC7_SetHigh()            do { LATCbits.LATC7 = 1; } while(0)
+#define IO_RC7_SetLow()             do { LATCbits.LATC7 = 0; } while(0)
+#define IO_RC7_Toggle()             do { LATCbits.LATC7 = ~LATCbits.LATC7; } while(0)
+#define IO_RC7_GetValue()           PORTCbits.RC7
+#define IO_RC7_SetDigitalInput()    do { TRISCbits.TRISC7 = 1; } while(0)
+#define IO_RC7_SetDigitalOutput()   do { TRISCbits.TRISC7 = 0; } while(0)
+#define IO_RC7_SetPullup()          do { WPUCbits.WPUC7 = 1; } while(0)
+#define IO_RC7_ResetPullup()        do { WPUCbits.WPUC7 = 0; } while(0)
+#define IO_RC7_SetPushPull()        do { ODCONCbits.ODCC7 = 0; } while(0)
+#define IO_RC7_SetOpenDrain()       do { ODCONCbits.ODCC7 = 1; } while(0)
+#define IO_RC7_SetAnalogMode()      do { ANSELCbits.ANSC7 = 1; } while(0)
+#define IO_RC7_SetDigitalMode()     do { ANSELCbits.ANSC7 = 0; } while(0)
+/**
+ * @ingroup  pinsdriver
+ * @brief GPIO and peripheral I/O initialization
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @ingroup  pinsdriver
+ * @brief Interrupt on Change Handling routine
+ * @param none
+ * @return none
+ */
+void PIN_MANAGER_IOC(void);
+
+
+#endif // PINS_H
+/**
+ End of File
+*/
