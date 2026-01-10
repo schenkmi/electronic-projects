1c0e9a82e4448d5e506e7d94fcd5de8669d72642
Michael Schenk <michaelschenk72@gmail.com>
2026-01-10 19:19:32 +0100

    v4 MCC update

---
+++ dac-pcm1792a/software/asrc-pcm1792a-v4.X/mcc_generated_files/system/src/config_bits.c
@@ -0,0 +1,77 @@
+/**
+ * Configuration Bits Generated Driver Source File
+ * 
+ * @file config_bits.c
+ * 
+ * @ingroup config_bitsdriver
+ * 
+ * @brief This file contains the API Implementation for the Device Configuration Bits driver.
+ *
+ * @version Driver Version 2.0.3
+ *
+ * @version Package Version 1.0.6
+*/
+
+/*
+? [2026] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+// Configuration bits: selected in the GUI
+
+//CONFIG1
+#pragma config FEXTOSC = OFF    // External Oscillator Selection bits->Oscillator not enabled
+#pragma config RSTOSC = HFINT32    // Reset Oscillator Selection bits->HFINTOSC with OSCFRQ= 32 MHz and CDIV = 1:1
+#pragma config CLKOUTEN = OFF    // Clock Out Enable bit->CLKOUT function is disabled; i/o or oscillator function on OSC2
+#pragma config CSWEN = ON    // Clock Switch Enable bit->Writing to NOSC and NDIV is allowed
+#pragma config FCMEN = ON    // Fail-Safe Clock Monitor Enable bit->FSCM timer enabled
+
+//CONFIG2
+#pragma config MCLRE = ON    // Master Clear Enable bit->MCLR pin is Master Clear function
+#pragma config PWRTS = OFF    // Power-up Timer Selection bits->PWRT disabled
+#pragma config LPBOREN = OFF    // Low-Power BOR Enable bit->ULPBOR disabled
+#pragma config BOREN = ON    // Brown-out Reset Enable bits->Brown-out Reset Enabled, SBOREN bit is ignored
+#pragma config BORV = LO    // Brown-out Reset Voltage Selection bit->Brown-out Reset Voltage (VBOR) set to 2.45V
+#pragma config ZCD = OFF    // ZCD Disable bit->Zero-cross detect circuit is disabled at POR.
+#pragma config PPS1WAY = ON    // PPSLOCKED One-Way Set Enable bit->The PPSLOCK bit can be cleared and set only once in software
+#pragma config STVREN = ON    // Stack Overflow/Underflow Reset Enable bit->Stack Overflow or Underflow will cause a reset
+
+//CONFIG3
+#pragma config WDTCPS = WDTCPS_31    // WDT Period Select bits->Divider ratio 1:65536; software control of WDTPS
+#pragma config WDTE = OFF    // WDT Operating Mode bits->WDT Disabled, SWDTEN is ignored
+#pragma config WDTCWS = WDTCWS_7    // WDT Window Select bits->window always open (100%); software control; keyed access not required
+#pragma config WDTCCS = SC    // WDT Input Clock Select bits->Software Control
+
+//CONFIG4
+#pragma config BBSIZE = BB512    // Boot Block Size Selection bits->512 words boot block size
+#pragma config BBEN = OFF    // Boot Block Enable bit->Boot Block disabled
+#pragma config SAFEN = OFF    // Storage Area Flash (SAF) Enable bit->SAF disabled
+#pragma config WRTAPP = OFF    // Application Block Write Protection bit->Application Block not write protected
+#pragma config WRTB = OFF    // Boot Block Write Protection bit->Boot Block not write protected
+#pragma config WRTC = OFF    // Configuration Register Write Protection bit->Configuration Register not write protected
+#pragma config WRTD = OFF    // Data EEPROM Write Protection bit->Data EEPROM NOT write protected
+#pragma config WRTSAF = OFF    // Storage Area Flash (SAF) Write Protection bit->SAF not write protected
+#pragma config LVP = ON    // Low Voltage Programming Enable bit->Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.
+
+//CONFIG5
+#pragma config CP = OFF    // Program Flash Memory Code Protection bit->UserNVM code protection disabled
+
+/**
+ End of File
+*/
